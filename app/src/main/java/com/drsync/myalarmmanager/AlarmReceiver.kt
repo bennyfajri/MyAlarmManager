53f159760558d8e047206be1c5214bc32e6b5c1a
Muhammad Beni Fajri <67430854+bennyfajri@users.noreply.github.com>
2022-01-25 02:15:52 +0700

    set repeating alarm

--- app/src/main/java/com/drsync/myalarmmanager/AlarmReceiver.kt
+++ app/src/main/java/com/drsync/myalarmmanager/AlarmReceiver.kt
@@ -40,18 +40,25 @@ class AlarmReceiver : BroadcastReceiver() {
         val title = if(type.equals(TYPE_ONE_TIME, ignoreCase = true)) TYPE_ONE_TIME else TYPE_REPEATING
         val notifId = if(type.equals(TYPE_ONE_TIME, ignoreCase = true)) ID_ONETIME else ID_REPEATING
 
+
+        //Jika Anda ingin menampilkan dengan toast anda bisa menghilangkan komentar pada baris dibawah ini.
+//        showToast(context, title, message)
+
         showToast(context, title, message)
         if (message != null) {
             showAlarmNotification(context, title, message, notifId)
         }
     }
 
+    // Gunakan metode ini untuk menampilkan toast
     private fun showToast(context: Context, title: String, message: String?) {
         Toast.makeText(context, "$title : $message", Toast.LENGTH_LONG).show()
     }
 
+    // Metode ini digunakan untuk menjalankan alarm one time
     fun setOneTimeAlarm(context: Context, type: String, date: String, time: String, message: String) {
 
+        // Validasi inputan date dan time terlebih dahulu
         if (isDateInvalid(date, DATE_FORMAT) || isDateInvalid(time, TIME_FORMAT)) return
 
         val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
@@ -77,6 +84,39 @@ class AlarmReceiver : BroadcastReceiver() {
         Toast.makeText(context, "One time alarm set up", Toast.LENGTH_SHORT).show()
     }
 
+    // Metode ini digunakan untuk menjalankan alarm repeating
+    fun setRepeatingAlarm(context: Context, type: String, time: String, message: String) {
+
+        // Validasi inputan waktu terlebih dahulu
+        if(isDateInvalid(time, TIME_FORMAT)) return
+
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(context, AlarmReceiver::class.java)
+        intent.putExtra(EXTRA_MESSAGE, message)
+        intent.putExtra(EXTRA_TYPE, type)
+
+        val timeArray = time.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(timeArray[0]))
+        calendar.set(Calendar.MINUTE, Integer.parseInt(timeArray[1]))
+        calendar.set(Calendar.SECOND, 0)
+
+        val pendingIntent = PendingIntent.getBroadcast(context, ID_REPEATING, intent, 0)
+        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, AlarmManager.INTERVAL_DAY, pendingIntent)
+
+        Toast.makeText(context, "Repeating alarm set up", Toast.LENGTH_SHORT).show()
+    }
+
+    fun isAlarmSet(context: Context, type: String): Boolean {
+        val intent = Intent(context, AlarmReceiver::class.java)
+        val requestCode = if(type.equals(TYPE_ONE_TIME, ignoreCase = true)) ID_ONETIME else ID_REPEATING
+
+        return PendingIntent.getBroadcast(context, requestCode, intent, PendingIntent.FLAG_NO_CREATE) != null
+    }
+
+
+    // Metode ini digunakan untuk validasi date dan time
     private fun isDateInvalid(date: String, format: String): Boolean {
         return try {
             val df = SimpleDateFormat(format, Locale.getDefault())
@@ -88,6 +128,7 @@ class AlarmReceiver : BroadcastReceiver() {
         }
     }
 
+    // Gunakan metode ini untuk menampilkan notifikasi
     private fun showAlarmNotification(context: Context, title: String,message: String, notifId: Int) {
 
         val channelId = "Channel_1"
@@ -103,7 +144,14 @@ class AlarmReceiver : BroadcastReceiver() {
             .setVibrate(longArrayOf(1000, 1000, 1000, 1000, 1000))
             .setSound(alarmSound)
 
+        /*
+          Untuk android Oreo ke atas perlu menambahkan notification channel
+          Materi ini akan dibahas lebih lanjut di modul extended
+       */
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            /* Create or update. */
             val channel = NotificationChannel(channelId,
                 channelName,
                 NotificationManager.IMPORTANCE_DEFAULT)
